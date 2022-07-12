5b67990bd77a92571a3e9ac2e22edd94fe13fadf
Jaehyuksssss <ljh95310@naver.com>
2022-07-12 19:32:23 +0900

    ADD: 인피니티 스크롤 구현

--- my_blog/src/components/Main/PostList.tsx
+++ my_blog/src/components/Main/PostList.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent, useMemo } from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from 'components/Main/PostItem'
 import { PostListItemType } from 'types/PostItem.types'
+import useInfiniteScroll from 'hooks/useInfiniteScroll'
 
 export type PostType = {
   node: {
@@ -41,24 +42,16 @@ const PostListWrapper = styled.div`
 const PostList: FunctionComponent<PostListProps> = function ({
   selectedCategory,
   posts,
-}) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostListItemType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
-  )
+}) { 
+  const {containerRef,postList} = useInfiniteScroll(
+    selectedCategory,
+    posts
+    )
+
+   
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
+    <PostListWrapper ref ={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
         <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
       ))}
     </PostListWrapper>
