32e20d2a2810f6bc64f9a1914f9332b8cb745ddf
Jaehyuksssss <ljh9510@naver.com>
2022-07-12 17:37:30 +0900

    ADD: 메인페이지에 실제 데이터 띄어보기

--- my_blog/src/components/Main/PostItem.tsx
+++ my_blog/src/components/Main/PostItem.tsx
@@ -1,15 +1,10 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { PostFrontmatterType } from 'types/PostItem.types'
 
-type PostItemProps = {
-  title: string
-  date: string
-  categories: string[]
-  summary: string
-  thumbnail: string
-  link: string
-}
+type PostItemProps = PostFrontmatterType & { link: string }
 
 const PostItemWrapper = styled(Link)`
   display: flex;
@@ -24,12 +19,12 @@ const PostItemWrapper = styled(Link)`
   }
 `
 
-const ThumbnailImage = styled.img`
+const ThumbnailImage = styled(GatsbyImage)`
   width: 100%;
   height: 200px;
   border-radius: 10px 10px 0 0;
-  object-fit: cover;
 `
+
 const PostItemContent = styled.div`
   flex: 1;
   display: flex;
@@ -85,28 +80,29 @@ const Summary = styled.div`
   opacity: 0.8;
 `
 
-
 const PostItem: FunctionComponent<PostItemProps> = function ({
   title,
   date,
   categories,
   summary,
-  thumbnail,
+  thumbnail: {
+    childImageSharp: { gatsbyImageData },
+  },
   link,
 }) {
   return (
-    <PostItemWrapper to= {link}>
-      <ThumbnailImage src={thumbnail} alt="Post Item Image" />
-
-      <PostItemContent><Title>{title}</Title></PostItemContent>
-      <Date>{date}</Date>
-
-      <Category>
-          {categories.map(category => (
-            <CategoryItem key={category}>{category}</CategoryItem>
+    <PostItemWrapper to={link}>
+      <ThumbnailImage image={gatsbyImageData} alt="Post Item Image" />
+      <PostItemContent>
+        <Title>{title}</Title>
+        <Date>{date}</Date>
+        <Category>
+          {categories.map(item => (
+            <CategoryItem key={item}>{item}</CategoryItem>
           ))}
         </Category>
         <Summary>{summary}</Summary>
+      </PostItemContent>
     </PostItemWrapper>
   )
 }
