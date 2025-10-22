dc3986da6881e2c5476093251e8367c869442409
jaehyuksssss <ljh95310@naver.com>
2025-10-22 16:45:34 +0900

    modify: 구글 광고로 변경

--- my_blog/gatsby-ssr.js
+++ my_blog/gatsby-ssr.js
@@ -4,4 +4,19 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// Google AdSense 메타 태그 제거 - Kakao AdFit으로 변경
+const React = require("react")
+
+// Inject the Google AdSense account verification meta tag on every page
+exports.onRenderBody = ({ setHeadComponents }) => {
+  const adsenseClient =
+    process.env.GATSBY_ADSENSE_CLIENT || "ca-pub-3398641306673607"
+  if (!adsenseClient) return
+
+  setHeadComponents([
+    React.createElement("meta", {
+      key: "google-adsense-account",
+      name: "google-adsense-account",
+      content: adsenseClient,
+    }),
+  ])
+}
