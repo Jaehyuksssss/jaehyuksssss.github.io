7b86a22828e0a8cee395eb347255a4c96f97bb71
jaehyuksssss <ljh95310@naver.com>
2025-10-22 15:38:44 +0900

    ADD : add.tsx

--- my_blog/gatsby-ssr.js
+++ my_blog/gatsby-ssr.js
@@ -4,18 +4,4 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-const React = require('react')
-
-// Inject the AdSense account verification meta tag on every page
-exports.onRenderBody = ({ setHeadComponents }) => {
-  const adsenseClient = process.env.GATSBY_ADSENSE_CLIENT || 'ca-pub-3398641306673607'
-  if (!adsenseClient) return
-
-  setHeadComponents([
-    React.createElement('meta', {
-      key: 'google-adsense-account',
-      name: 'google-adsense-account',
-      content: adsenseClient,
-    }),
-  ])
-}
+// Google AdSense 메타 태그 제거 - Kakao AdFit으로 변경
