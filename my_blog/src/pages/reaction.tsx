7ec05f893b2b518e39ae2ba8a2237c8b1321ce7f
jaehyuksssss <ljh95310@naver.com>
2025-10-23 15:23:12 +0900

    ADD : game

--- my_blog/src/pages/reaction.tsx
+++ my_blog/src/pages/reaction.tsx
@@ -4,11 +4,10 @@ import ReactionGame from 'components/Game/ReactionGame'
 
 const ReactionPage: React.FC = () => {
   return (
-    <Template title="반응속도 테스트" description="색이 다른 칸을 빠르게 찾아 클릭하는 반응속도 게임" url="/reaction">
-      <ReactionGame rounds={5} grid={4} />
+    <Template title="반응속도 테스트" description="30초 타임어택: 색이 다른 칸을 빠르게 찾아 클릭" url="/reaction">
+      <ReactionGame />
     </Template>
   )
 }
 
 export default ReactionPage
-
